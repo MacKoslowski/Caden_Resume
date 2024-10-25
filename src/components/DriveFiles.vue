f79bd954d30627181d46ed8fe945e2e1d22f0f6f
MacKoslowski <macleankoslowski@gmail.com>
2024-10-24 20:47:52 -0400

    added pdf links maually

--- src/components/DriveFiles.vue
+++ src/components/DriveFiles.vue
@@ -1,31 +1,21 @@
 <template>
   <div>
     <div class="text-3xl w-full font-bold mb-2">Portfolio PDF Links</div>
-    <!--<ul>
-      <li v-for="file in files" :key="file.id">
-        <a
-          :href="`https://drive.google.com/uc?id=${file.id}`"
-          target="_blank"
-          >{{ file.name }}</a
+    <ul>
+      <li>
+        <a href="/Diabetes_Weight_Loss_drug_log.pdf" target="_blank"
+          >Diabetes Weight Loss Drug Blog Post</a
         >
       </li>
-    </ul>-->
-
-    <div
-      v-for="file in files"
-      :key="file.id"
-      class="rounded-lg bg-base-100 hover:shadow-xl transition ease-in-out hover:scale-[102%]"
-    >
-      <a :href="`https://drive.google.com/uc?id=${file.id}`" target="_blank">
-        <div class="hero-content flex-col md:flex-row">
-          <div class="grow w-full">
-            <h1 class="text-xl font-bold">
-              {{ file.name }}
-            </h1>
-          </div>
-        </div>
-      </a>
-    </div>
+      <li>
+        <a href="Everyday_Flowers_July.pdf" target="_blank">Everyday Flowers</a>
+      </li>
+      <li>
+        <a href="Jewlery_Lab_Diamonds.pdf" target="_blank"
+          >Jewlery Lab Diamonds</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -38,22 +28,6 @@ export default {
       files: [],
     };
   },
-  mounted() {
-    //const apiKey = "AIzaSyASxw8DT8Gt7z6bIbG5ixw6LmJw6_x7YF8";
-    const folderId = "1TOPTFSZdPXjXspSu45tSK40Hn8cinbi4";
-
-    const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents`; //&key=${apiKey}
-
-    axios
-      .get(url)
-      .then((response) => {
-        this.files = response.data.files.filter(
-          (file) => file.mimeType === "application/pdf"
-        );
-      })
-      .catch((error) => {
-        console.error("Error fetching files:", error);
-      });
-  },
+  mounted() {},
 };
 </script>
